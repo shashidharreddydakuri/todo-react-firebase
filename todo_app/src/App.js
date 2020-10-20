d1c23edadba45afa7e19f32ea6b1ca195ee70724
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2020-10-20 13:41:18 +0530

    retrieved from firebase

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from './Todo';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 
 import './App.css';
+import db from './firebase';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
   // console.log(input);
+
+  useEffect(() => {
+    //this code here... fires when the app.js loads
+    db.collection('todos').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, []);
   
   const addTodo = (event) => {
 
@@ -19,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Todo App</h1>
       <form>        
         <FormControl>
           <InputLabel>write a Todo</InputLabel>
